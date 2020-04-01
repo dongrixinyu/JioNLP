6a31dc6ff1f5eb28feb56a4a7762f3ea4be5ef63
cuichengyu <cuichengyu@bbdsevice.com>
2020-04-01 17:33:46 +0800

    add new func init

--- jionlp/gadget/__init__.py
+++ jionlp/gadget/__init__.py
@@ -6,12 +6,14 @@
 from .money_standardization import MoneyStandardization
 from .split_sentence import SplitSentence
 from .id_card_parser import IDCardParser
+from .location_parser import LocationParser
 from jionlp.util.fast_loader import FastLoader
 
 
 money_standardization = MoneyStandardization()
 id_card_parser = IDCardParser()
 split_sentence = SplitSentence()
+location_parser = LocationParser()
 
 
 #rule = FastLoader('rule', globals(), 'jionlp.rule')
