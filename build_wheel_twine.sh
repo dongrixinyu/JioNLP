6da9b897963de85e96284719feeb22fef4c737a8
冬日新雨 <dongrixinyu.89@163.com>
2022-07-24 23:56:27 +0800

    Update build_wheel_twine.sh

--- build_wheel_twine.sh
+++ build_wheel_twine.sh
@@ -13,6 +13,31 @@ if [ -d jiojio.egg-info ]; then
     rm -rf jiojio.egg-info
 fi
 
+to_be_deleted = (
+  "char_distribution.json",
+  "china_location.txt",
+  "chinese_char_dictionary.txt",
+  "chinese_idiom.txt",
+  "chinese_word_dictionary.txt",
+  "idf.txt",
+  "phone_location.txt",
+  "pinyin_phrase.txt",
+  "pornography.txt",
+  "sentiment_words.txt",
+  "topic_word_weight.json",
+  "word_distribution.json",
+  "word_topic_weight.json",
+  "xiehouyu.txt"
+  )
+for item in "${to_be_deleted[@]}";
+do
+    if [ -d ./jionlp/dictionary/${item} ]; then
+        echo "$item"
+        rm -rf ./jionlp/dictionary/${item}
+fi
+done
+
+# char_distribution.json
 python3 setup.py bdist_wheel --universal
 
 pip install twine
