949ea093d3b4b02d27b6968e11858fcf0841c213
冬日新雨 <dongrixinyu.89@163.com>
2023-08-01 16:58:54 +0800

    update clean_html

--- jionlp/rule/html_cleansing.py
+++ jionlp/rule/html_cleansing.py
@@ -24,6 +24,7 @@ class CleanHTML(object):
         self.script_tag_pattern = re.compile(SCRIPT_TAG_PATTERN)
         self.style_tag_pattern = re.compile(STYLE_TAG_PATTERN)
         self.table_tag_pattern = re.compile(TABLE_TAG_PATTERN)
+        self.form_tag_pattern = re.compile(FORM_TAG_PATTERN)
         self.unordered_list_tag_pattern = re.compile(UNORDERED_LIST_TAG_PATTERN)
         self.ordered_list_tag_pattern = re.compile(ORDERED_LIST_TAG_PATTERN)
         self.footer_tag_pattern = re.compile(FOOTER_TAG_PATTERN)
@@ -33,10 +34,22 @@ class CleanHTML(object):
         self.comment_tag_pattern = re.compile(COMMENT_TAG_PATTERN)
         self.break_line_tag_pattern = re.compile(BREAK_LINE_TAG_PATTERN)
 
+        self.div_tag_start_pattern = re.compile(DIV_TAG_START_PATTERN)
+        self.div_tag_end_pattern = re.compile(DIV_TAG_END_PATTERN)
+
+        self.div_attr_remove_list = [
+            'menu', 'nav',
+            'header', 'footer',
+            'archive', 'bloglist', 'blog-list',
+            'sidebar', 'side-bar',
+            'cookie']
+
         self.html_tag_pattern = re.compile(HTML_TAG_PATTERN)
         self.tab_new_line_pattern = re.compile(TAB_NEW_LINE_PATTERN)
 
+        self.id_attr_pattern = re.compile(ID_ATTR_PATTERN)
         self.name_attr_pattern = re.compile(NAME_ATTR_PATTERN)
+        self.class_attr_pattern = re.compile(CLASS_ATTR_PATTERN)
         self.content_attr_pattern = re.compile(CONTENT_ATTR_PATTERN)
 
         self.add_new_line_pattern = re.compile(ADD_NEW_LINE_PATTERN)
@@ -62,19 +75,29 @@ class CleanHTML(object):
         if self.html_pattern is None:
             self._prepare()
 
+        # 对原始 html 文件直接去除其中多余的 \t \n 空格 等。
+        orig_html_text, _ = self.tab_new_line_pattern.subn('', orig_html_text)
+
+        # 获取 html 的元信息
         meta_info = self.extract_meta_info(orig_html_text)
 
         # remove <script>...</script> content, because these tags contain javascript text.
         html_text, _ = self.script_tag_pattern.subn('', orig_html_text)
         html_text, _ = self.style_tag_pattern.subn('', html_text)
         html_text, _ = self.table_tag_pattern.subn('', html_text)
-        html_text, _ = self.ordered_list_tag_pattern.subn('', html_text)
-        html_text, _ = self.unordered_list_tag_pattern.subn('', html_text)
+        html_text, _ = self.form_tag_pattern.subn('', html_text)
+        # html_text, _ = self.ordered_list_tag_pattern.subn('', html_text)
+        # html_text, _ = self.unordered_list_tag_pattern.subn('', html_text)
         html_text, _ = self.footer_tag_pattern.subn('', html_text)
-        # html_text, _ = self.navigation_tag_pattern.subn('', html_text)
 
         html_text, _ = self.meta_tag_pattern.subn('', html_text)
         html_text, _ = self.comment_tag_pattern.subn('', html_text)
+
+        # 去除那些带有导航的 div 标签
+        while True:
+            html_text, start_offset, tuned = self.remove_menu_div_tag(html_text)
+            if not tuned:
+                break
 
         # 将 <br> 替换为换行符 \n
         html_text, _ = self.break_line_tag_pattern.subn('\n', html_text)
@@ -89,9 +112,9 @@ class CleanHTML(object):
             content = content.replace(key, val)
 
         # 清除其中多余的 符号。
-        content, _ = self.tab_new_line_pattern.subn('\n\n', content)
-        print(html_text)
-        print(content)
+        # content, _ = self.tab_new_line_pattern.subn('\n\n', content)
+        # print(html_text)
+        # print(content)
         return content, meta_info
 
     def extract_meta_info(self, html_text):
@@ -112,11 +135,12 @@ class CleanHTML(object):
         for item in matched_res:
             matched_attr = self.name_attr_pattern.search(item)
             if matched_attr is not None:
-                matched_attr = matched_attr.group(2)
+                matched_attr = matched_attr.group('TagName')
                 try:
-                    matched_content = self.content_attr_pattern.search(item).group(2)
+                    matched_content = self.content_attr_pattern.search(item).group('TagContent')
                 except:
                     continue
+
                 if matched_attr == 'description':
                     meta_info.update({'description': matched_content})
                 elif matched_attr == 'keywords':
@@ -128,6 +152,101 @@ class CleanHTML(object):
 
         return meta_info
 
+    def remove_menu_div_tag(self, html_text):
+        """删除那些表示 menu、navigation 等信息的 div tag。
 
+        Args:
+            html_text: HTML 文档
 
+        Returns: html 文档
+
+        """
+        start_offset = 0
+        matched_item = None
+
+        for idx, matched_item in enumerate(self.div_tag_start_pattern.finditer(html_text)):
+            # 检查第一个 div 的属性 id class，如果有 menu，则剔除该部分。
+            id_attr_res = self.id_attr_pattern.search(matched_item.group())
+            class_attr_res = self.class_attr_pattern.search(matched_item.group())
+
+            # 判断是否匹配到 menu nav 等 div 标签
+            tuned_flag = False
+            for keyword in self.div_attr_remove_list:
+                if id_attr_res:
+                    if keyword in id_attr_res.group('TagID').lower():
+                        tuned_flag = True
+                        break
+
+                if class_attr_res:
+                    if keyword in class_attr_res.group('TagClass').lower():
+                        tuned_flag = True
+                        break
+
+            if tuned_flag:
+                start_offset = matched_item.span()[1]
+                break
+
+        if start_offset != 0:
+            nested_list = []
+            end_offset = 0
+            while True:
+                start_res = self.div_tag_start_pattern.search(html_text[start_offset:])
+                end_res = self.div_tag_end_pattern.search(html_text[start_offset:])
+
+                # check matched res：
+                if start_res is not None and end_res is not None:
+
+                    if start_res.span()[0] < end_res.span()[0]:
+                        nested_list.append(start_res)
+                        start_offset = start_res.span()[1] + start_offset
+                    elif start_res.span()[0] > end_res.span()[0]:
+                        if len(nested_list) > 0:
+                            nested_list.pop()
+                            if len(nested_list) == 0:
+                                end_offset = end_res.span()[1] + start_offset
+                                break
+                            else:
+                                start_offset = end_res.span()[1] + start_offset
+                        else:
+                            # 初始 div 元素下，没有别的 div 子嵌套
+                            end_offset = start_offset
+                            break
+
+                    else:
+                        break
+
+                elif start_res is not None and end_res is None:
+                    break
+
+                elif start_res is None and end_res is not None:
+                    if len(nested_list) > 0:
+
+                        nested_list.pop()
+                        if len(nested_list) == 0:
+                            end_offset = end_res.span()[1] + start_offset
+                            break
+                        else:
+                            start_offset = end_res.span()[1] + start_offset
+                    else:
+                        # 初始 div 元素下，没有别的 div 子嵌套
+                        end_offset = start_offset
+                        break
+
+                elif start_res is None and end_res is None:
+                    break
+
+            if end_offset != 0:
+                # 找见初次 div 的 </div> 标签
+                end_res = self.div_tag_end_pattern.search(html_text[end_offset:])
+                if end_res is not None:
+                    end_offset = end_res.span()[1] + end_offset
+
+                html_text = html_text[:matched_item.span()[0]] + html_text[end_offset:]
+
+                return html_text, matched_item.span()[0], True
+
+            else:
+                return html_text, matched_item.span()[0], False
+        else:
+            return html_text, 0, False
 
--- jionlp/rule/html_rule_pattern.py
+++ jionlp/rule/html_rule_pattern.py
@@ -12,9 +12,15 @@ from jionlp.util.funcs import absence
 
 # ---------------------------------------------------------------------
 # HTML 格式文档正则
+
+DIV_TAG_START_PATTERN = '(<(div|DIV)[^<>]*>)'
+DIV_TAG_END_PATTERN = '(</(div|DIV)>)'
+
+
 SCRIPT_TAG_PATTERN = '(<script(.|\n)*?>(.|\n)*?</script>|<SCRIPT(.|\n)*?>(.|\n)*?</SCRIPT>)'
 STYLE_TAG_PATTERN = '(<style(.|\n)*?>(.|\n)*?</style>|<STYLE(.|\n)*?>(.|\n)*?</STYLE>)'
 TABLE_TAG_PATTERN = '(<table(.|\n)*?>(.|\n)*?</table>|<TABLE(.|\n)*?>(.|\n)*?</TABLE>)'
+FORM_TAG_PATTERN = '(<form(.|\n)*?>(.|\n)*?</form>|<FORM(.|\n)*?>(.|\n)*?</FORM>)'
 ORDERED_LIST_TAG_PATTERN = '(<ol(.|\n)*?>(.|\n)*?</ol>|<OL(.|\n)*?>(.|\n)*?</OL>)'
 UNORDERED_LIST_TAG_PATTERN = '(<ul(.|\n)*?>(.|\n)*?</ul>|<UL(.|\n)*?>(.|\n)*?</UL>)'
 FOOTER_TAG_PATTERN = '(<footer(.|\n)*?>(.|\n)*?</footer>|<FOOTER(.|\n)*?>(.|\n)*?</FOOTER>)'
@@ -23,6 +29,11 @@ NAVIGATION_TAG_PATTERN = '(<nav(.|\n)*?>(.|\n)*?</nav>|<NAV(.|\n)*?>(.|\n)*?</NA
 META_TAG_PATTERN = '<meta.*?>'
 COMMENT_TAG_PATTERN = '<!--(.|\n)*?-->'
 BREAK_LINE_TAG_PATTERN = '(<br>|<BR>)'
+
+# ---------------------------------------------------------------------
+# 带属性信息的 element 标签
+# 某些 div 标签是导航栏，应当去掉。主要通过 div 的 class 属性与 id 属性来完成。
+# DIV_NAVIGATION_TAG_PATTERN = ''
 
 
 HTML_TAG_STRICT_PATTERN = \
@@ -37,9 +48,14 @@ ADD_NEW_LINE_PATTERN = r'(</p>|</h1>|</h2>|</h3>|</h4>|</h5>)'
 
 # ---------------------------------------------------------------------
 # 属性正则
-NAME_ATTR_PATTERN = '(name="([^"]*)"|name=\'([^\']*)\')'
-CONTENT_ATTR_PATTERN = '(content="([^"]*)"|content=\'([^\']*)\')'
+NAME_ATTR_PATTERN = '[NAME][name]=(?P<Quote>["\']?)(?P<TagName>[\w\-] *?)(?P=Quote)'
+CONTENT_ATTR_PATTERN = '[Content][content]=(?P<Quote>["\']?)(?P<TagContent>[\w\- ]*?)(?P=Quote)'
+
+ID_ATTR_PATTERN = '[iI][dD]=(?P<Quote>["\']?)(?P<TagID>[\w\- ]*?)(?P=Quote)'
+CLASS_ATTR_PATTERN = '(CLASS|class)=(?P<Quote>["\']?)(?P<TagClass>[\w\- ]*?)(?P=Quote)'
 
 # ---------------------------------------------------------------------
 # 冗余空格正则
 TAB_NEW_LINE_PATTERN = '[\n\t ]{1,}\n[\n\t ]{1,}'
+
+TAB_NEW_LINE_PATTERN = '[\n\t]+'
